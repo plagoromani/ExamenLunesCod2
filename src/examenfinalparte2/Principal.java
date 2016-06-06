c3b45448548cdcd7d847bf1cf4fbfc608e249133
Pablite5 <Pablite5@GAMING>
2016-06-06 09:53:17 +0200

    más cambios

--- src/examenfinalparte2/Principal.java
+++ src/examenfinalparte2/Principal.java
@@ -16,7 +16,7 @@ public class Principal {
     public static void main(String arg[]){
         /**
          * Aqui pedimos por pantalla que ingresemos el numero de digitos que queremos que se nos muestre.
-         * @param ndig es igual a 0 para que nos empiece a contar desde 0
+         * @param ndig es igual a 0 para que no tome otro valor.
          */
         
         int dig= Integer.parseInt(JOptionPane.showInputDialog("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): "));
@@ -32,7 +32,7 @@ public class Principal {
         
         /**
          * Se crea un for para recorrer el bucle.
-         * @param aux es un parámetro que se iguala a i para que sea distinto de cero.
+         * @param aux es un parámetro que tenemos que crear, ya que si fuese i el resultado sería distinto
          */
         for(int i = 1; i <= 99999; i++ )
         {
@@ -44,7 +44,12 @@ public class Principal {
             aux = aux / 10;
             contador++;
         }
-        
+        /**
+         * Se procede a realizar las operaciones del bucle.
+         * Aquí el bucle decirirá si mostrarlo luego o no.
+         * Si es igual a true se mostrará.
+         * Si es igual a false no se mostrará nada.
+         */
             ndig=contador;
             
             
@@ -70,7 +75,9 @@ public class Principal {
         if (contador1 == 1) p = true;
             } 
         } 
-     
+     /**
+      * Aquí se mostrará si el resultado es igual a true.
+      */
                 if (p == true) 
                     System.out.println(i);    
             }
